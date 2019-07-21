9a8f261173a395be941c3202d52e1df99ae859f8
Deepak Sharma <sharma.deepak192@gmail.com>
2019-07-20 23:49:15 -0700

    from 3 start

--- controller/borrowerRegister2Ctrl.js
+++ controller/borrowerRegister2Ctrl.js
@@ -1,29 +1,35 @@
 app.controller("borrowerRegister2Ctrl", function ($scope,$location) {
    $scope.formData =
   {
-    Loan_amount:"",
-    Loan_tenure:"",
-    Loan_Purpose:"Medical Emergency",
-    Loan_Affordable:"",
-    Gender:"Male",
-    Marital:"Single",
-    Spouse_Working:"No",
-    Spouse_Monthly_Income:"",
-    Working_Parents:"Yes",
-    Parent_Monthly_Income:"",
-    other_source:"No",
-    other_Income_Amount:"",
-    Education:"Graduation",
-    Kids:""	    
+    houseType:"Owned",
+    rent:"",  
+    address:{
+    	address1:"" ,
+	    address2:"" ,
+	    pincode:"" ,
+	    City:"",
+	    State:"",
+	    Country:""
+    },
+    Details:{
+    	Employmenttype:"Salaried",
+    	Business:"Business",
+    	FirmName:"",
+    	FirmType:"FMCG 1",
+    	Designation:"",
+    	EmployeedSince:"",
+    	OfficialEmail:"",
+    }
+    
   }
    $scope.register = function(){
     var d = $scope.formData;
-    if(!d.Loan_amount){
-      alert("Enter Loan_amount.")
-    }else if(!d.Loan_tenure){
-      alert("Enter Loan_tenure.")
-    }else if(!d.Loan_Purpose){
-      alert("Enter Loan_Purpose.")
+    if(d.houseType == 'Rented' && d.rent == ""){
+      alert("Enter Rented.")
+    }else if(!d.address1){
+      alert("Enter address1.")
+    }else if(!d.pincode){
+      alert("Enter pincode.")
     }else if(!d.Loan_Affordable){
       alert("Enter Loan_Affordable EMI.")
     }else if(d.Marital == "Married" && d.Spouse_Working == "Yes" && d.Spouse_Monthly_Income ==""){
